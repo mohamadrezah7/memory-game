a1c87976c7dc54c3bbfd0bbb1343965e0941e2f4
mh-shahin <m.rezahossini@gmail.com>
2021-12-02 19:43:36 +0330

    first commit

---
+++ public/ServiceWorker.js
@@ -0,0 +1,39 @@
+const staticCacheName = "site-static-v1";
+const cacheAssets = [
+  "/",
+  "/index.html",
+  "/icon.png",
+  "/img/*",
+  "../src/App.js",
+  "../src/App.css",
+  "../src/index.css",
+  "../src/components/SingleCard.css",
+  "../src/components/SingleCard.js"
+];
+
+self.addEventListener("install", evt => {
+  evt.waitUntil(
+    caches
+      .open(staticCacheName)
+      .then(cache => {
+        console.log("caching assets...");
+        cache.addAll(cacheAssets);
+      })
+      .catch(err => {})
+  );
+});
+
+self.addEventListener("fetch", evt => {
+  evt.respondWith(
+    caches
+      .match(evt.request)
+      .then(res => {
+        return res || fetch(evt.request);
+      })
+      .catch(err => {
+        if (evt.request.url.indexOf(".html") > -1) {
+          return caches.match("./pages/fallback.html");
+        }
+      })
+  );
+});
---
+++ src/App.js
@@ -0,0 +1,103 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import SingleCard from './components/SingleCard';
+
+const cardImage = [
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false },
+]
+
+function App() {
+
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImage, ...cardImage]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random()}))
+
+
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setCards(shuffledCards);
+    setTurns(0);
+    }
+
+    // handle Choice
+    const handleChoice = (card) => {
+      choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+    }
+
+    // compare two card
+    useEffect(() => {
+      if(choiceOne && choiceTwo) {
+        setDisabled(true)
+        if(choiceOne.src === choiceTwo.src){
+          setCards(prevCards => {
+            return prevCards.map((card) => {
+              if(card.src === choiceOne.src){
+                return {...card, matched: true}
+              } else {
+                return card
+              }
+            })
+          })
+          resetTurn();
+        } else {
+          setTimeout(() => resetTurn(), 1000);
+        }
+      }
+    }, [choiceOne, choiceTwo])
+
+
+    // start a new  game automaticlly
+    useEffect(() => {
+      shuffleCards();
+    }, [])
+
+
+
+
+    // reset choice & increase turns
+    const resetTurn = () => {
+      setChoiceOne(null);
+      setChoiceTwo(null);
+      setTurns(prevTurns => prevTurns + 1)
+      setDisabled(false)
+    }
+  
+ 
+  return (
+    <div className="App">
+      <h1>Magic Memory Game</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard 
+            key={card.id} 
+            card={card}
+            handleChoice={handleChoice} 
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+      <p className="turns">Turn : {turns}</p>
+      <div className="footer">
+          Designed By Mohamadreza©
+      </div>
+    </div>
+  );
+}
+
+export default App;
